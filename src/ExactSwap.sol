231cbf1d44a53926ccffa324afd99ef52a67afc1
Rahu <rahunandan@gmail.com>
2024-07-30 21:55:52 +0800

    exact swap working

--- src/ExactSwap.sol
+++ src/ExactSwap.sol
@@ -6,6 +6,8 @@ import "./interfaces/IERC20.sol";
 import {console} from "forge-std/Test.sol";
 
 contract ExactSwap {
+    uint256 public constant usdcOut = 1337 * 1e6;
+
     /**
      *  PERFORM AN SIMPLE SWAP WITHOUT ROUTER EXERCISE
      *
@@ -26,56 +28,21 @@ contract ExactSwap {
 
         // your code start here
 
-        // dy = (xdx) / (0.997(x - dx))
-        // dy = (997 * x * dx) / (1000 * x - 997 * dx)
+        // dx = (xdy) / (0.997(y - dy))
 
-        // (uint256 r0, uint256 r1, ) = IUniswapV2Pair(pool).getReserves();
-        // (uint256 reserveIn, uint256 reserveOut) = IUniswapV2Pair(pool)
-        //     .getReserves();
-
-        (uint256 reserveA, uint256 reserveB, ) = IUniswapV2Pair(pool)
+        (uint256 reserveOut, uint256 reserveIn, ) = IUniswapV2Pair(pool)
             .getReserves();
 
-        console.log("reserveA, reserveB", reserveA, reserveB);
+        uint ethOutPut = (reserveIn * usdcOut * 1000) /
+            ((reserveOut - usdcOut) * 997) +
+            1;
 
-        address toeken0 = IUniswapV2Pair(pool).token0();
-        address toekn1 = IUniswapV2Pair(pool).token1();
-        // console.log("reserveIn, reserveOut", reserveIn, reserveOut);
-        console.log("token0, token1", toeken0, toekn1);
-
-        // uint256 x = IERC20(weth).balanceOf(address(this));
-        // uint256 dx = (x * 997 * r0) / (1000 * x + 997 * r1);
-        // console.log("dx", dx);
-        // console.log("r0, r1", r0, r1, x);
-        // uint amountOut = 1337 * 1e6;
-
-        // uint numerator = reserveIn * amountOut * 1000;
-        // // console.log("numerator", numerator);
-        // uint denominator = (reserveOut - amountOut) * 997;
-        // // console.log("denominator", denominator);
-        // uint amountIn = (numerator / denominator) + 1;
-        // console.log("amountIn", amountIn);
-
-        /*uint numerator = reserveIn.mul(amountOut).mul(1000);
-        uint denominator = reserveOut.sub(amountOut).mul(997);
-        amountIn = (numerator / denominator).add(1);*/
-
-        uint output = getAmountIn(1337 * 1e6, reserveB, reserveA);
-        console.log("output", output);
-
-        uint256 d = (output * 997 * reserveA) /
-            ((reserveB * 1000) + (997 * output));
-        console.log("d", d);
-
-        IUniswapV2Pair(weth).transfer(pool, output);
-        // IUniswapV2Pair(usdc).transfer(pool, 1337);
+        IUniswapV2Pair(weth).transfer(pool, ethOutPut);
         IUniswapV2Pair(pool).swap(1337 * 1e6, 0, address(this), "");
-
-        // uint input = getInputAmount(3500 * 1e6, reserveIn, reserveOut);
     }
 
     // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
-    function getAmountIn(
+    /*function getAmountIn(
         uint amountOut,
         uint reserveIn,
         uint reserveOut
@@ -88,5 +55,5 @@ contract ExactSwap {
         uint numerator = reserveIn * amountOut * 1000;
         uint denominator = (reserveOut - amountOut) * (997);
         amountIn = (numerator / denominator) + 1;
-    }
+    }*/
 }
--- test/ExactSwap.t.sol
+++ test/ExactSwap.t.sol
@@ -34,8 +34,6 @@ contract ExactSwapTest is Test {
 
         uint256 puzzleBal = IUniswapV2Pair(usdc).balanceOf(address(exactSwap));
 
-        console2.log("WETH Balance", foo);
-        console2.log("USDC Balance", puzzleBal);
         require(puzzleBal / 1e6 == 1337, "Puzzle Balance Not 1337 USDC.");
         require(d / 1e6 == 1337, "Did Not Swap Exact Amount Of WETH.");
     }
