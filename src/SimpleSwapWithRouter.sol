a8f978ca67ee5614195b2d0bffc8d32921b8a17b
Rahu <rahunandan@gmail.com>
2024-08-02 15:49:02 +0700

    SimpleSwapWithRouter

--- src/SimpleSwapWithRouter.sol
+++ src/SimpleSwapWithRouter.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "./interfaces/IUniswapV2Pair.sol";
+import {console2} from "forge-std/Test.sol";
 
 contract SimpleSwapWithRouter {
     /**
@@ -13,12 +14,31 @@ contract SimpleSwapWithRouter {
      */
     address public immutable router;
 
+    address public weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    // address public usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
     constructor(address _router) {
         router = _router;
     }
 
-    function performSwapWithRouter(address[] calldata path, uint256 deadline) public {
+    function performSwapWithRouter(
+        address[] calldata path,
+        uint256 deadline
+    ) public {
         // your code start here
+
+        IUniswapV2Pair(weth).balanceOf(address(this));
+        console2.log(
+            "weth balance: %d",
+            IUniswapV2Pair(weth).balanceOf(address(this))
+        );
+        IUniswapV2Router(router).swapExactETHForTokens{value: 1 ether}(
+            0,
+            path,
+            address(this),
+            deadline
+        );
     }
 
     receive() external payable {}
@@ -31,8 +51,10 @@ interface IUniswapV2Router {
      *     to: recipient address to receive the liquidity tokens.
      *     deadline: timestamp after which the transaction will revert.
      */
-    function swapExactETHForTokens(uint256 amountOutMin, address[] calldata path, address to, uint256 deadline)
-        external
-        payable
-        returns (uint256[] memory amounts);
+    function swapExactETHForTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
 }
